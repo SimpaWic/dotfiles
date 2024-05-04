f7ce86f225b71c80713166a45d47f48f26515b64
Simon Wictorsson <SimpaWic@users.noreply.github.com>
2024-05-04 19:46:05 +0200

    nvim mini conf

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = {
+      open = {
+        enable = false,
+      },
+      close = {
+        enable = false,
+      },
+    },
+  },
+}
