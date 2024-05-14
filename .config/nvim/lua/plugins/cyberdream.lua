088e6fdb759552e5c7e06bd36848092f4ea9e7d8
Simon Wictorsson <SimpaWic@users.noreply.github.com>
2024-05-14 10:47:00 +0200

    disabled

--- .config/nvim/lua/plugins/cyberdream.lua
+++ .config/nvim/lua/plugins/cyberdream.lua
@@ -1,21 +1,18 @@
-if true then
-  return {}
-end
 return {
   {
     "scottmckendry/cyberdream.nvim",
+    enabled = false, -- NOTE: disabled
     lazy = false,
     priority = 1000,
     config = function()
       require("cyberdream").setup({
-        -- Recommended - see "Configuring" below for more config options
         transparent = true,
         italic_comments = true,
         hide_fillchars = true,
         borderless_telescope = false,
         terminal_colors = true,
       })
-      vim.cmd("colorscheme cyberdream") -- set the colorscheme
+      -- vim.cmd("colorscheme cyberdream") -- set the colorscheme
     end,
   },
 }
