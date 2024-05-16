88285a71eee1f48e5126c61b6beb18ab1d09040a
Simon Wictorsson <SimpaWic@users.noreply.github.com>
2024-05-16 09:15:19 +0200

    auto show lsp signature off
    toggle show of lsp signature in insert with <C-x>

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,8 +1,11 @@
 return {
   "folke/noice.nvim",
-  opts = {
-    presets = {
-      bottom_search = false,
-    },
-  },
+  opts = function(_, opts)
+    opts.presets.bottom_search = false
+    opts.lsp.signature = {
+      auto_open = { enabled = false },
+    }
+
+    vim.keymap.set("i", "<C-x>", vim.lsp.buf.signature_help)
+  end,
 }
