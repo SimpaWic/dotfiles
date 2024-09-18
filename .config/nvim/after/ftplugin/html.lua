b0bc0380a82d292e70065b57014d78c0922bbb54
Simon Wictorsson <SimpaWic@users.noreply.github.com>
2024-09-18 09:56:47 +0200

    options for html files

---
+++ .config/nvim/after/ftplugin/html.lua
@@ -0,0 +1,7 @@
+-- options for html files
+
+local opt = vim.opt
+
+opt.expandtab = false -- Use tabs instead of spaces
+opt.shiftwidth = 4 -- Size of an indent
+opt.tabstop = 4 -- Number of spaces tabs count for
