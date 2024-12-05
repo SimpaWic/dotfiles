0fde97cadd283317a13d7eef7335c47ec6939422
Simon Wictorsson <SimpaWic@users.noreply.github.com>
2024-12-05 15:51:56 +0100

    ftplugin typescriptreact

---
+++ .config/nvim/after/ftplugin/typescriptreact.lua
@@ -0,0 +1,7 @@
+-- options for typescriptreact files
+
+local opt = vim.opt
+
+opt.expandtab = false -- Use tabs instead of spaces
+opt.shiftwidth = 2 -- Size of an indent
+opt.tabstop = 2 -- Number of spaces tabs count for
