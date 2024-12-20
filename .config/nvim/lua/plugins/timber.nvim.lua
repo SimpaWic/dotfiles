04a29a2265ae183ff2fd8ce7b1e9ad4ca0eebcea
Simon Wictorsson <SimpaWic@users.noreply.github.com>
2024-12-20 10:34:52 +0100

    try timber.nvim

---
+++ .config/nvim/lua/plugins/timber.nvim.lua
@@ -0,0 +1,14 @@
+return {
+  "Goose97/timber.nvim",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("timber").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+    require("which-key").add({
+      { "gl", desc = "log", icon = { icon = "" } },
+      { "gL", desc = "Log", icon = { icon = "" } },
+    })
+  end,
+}
