4c6f0e668bb711bf88fe77ffc0d6884cbbe64bd4
Simon Wictorsson <SimpaWic@users.noreply.github.com>
2024-12-20 10:30:55 +0100

    remove prettier extra

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -34,7 +34,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.util.dot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
     { import = "colorschemes" },
